a1e0c5d198a3c410f30e6a837ef29e5c636fa7e7
David <davidoh210@gmail.com>
2017-10-12 16:54:42 -0400

    clean up inject.js

--- practice_chrome_ext/inject.js
+++ practice_chrome_ext/inject.js
@@ -11,46 +11,4 @@
 	div.textContent = 'Injected!';
 	document.body.appendChild(div);
 
-	//alert('inserted self... giggity');
-	while (true) {
-	    function leftArrowPressed() {
-	    	div.style.left = parseInt(div.style.left) - 5 + 'px';
-	    }
-
-	    function rightArrowPressed() {
-	    	div.style.left = parseInt(div.style.left) + 5 + 'px';
-	    }
-
-	    function upArrowPressed() {
-	    	div.style.top = parseInt(div.style.top) - 5 + 'px';
-	    }
-
-	    function downArrowPressed() {
-	    	div.style.top = parseInt(div.style.top) + 5 + 'px';
-	    }
-
-	    function moveSelection(evt) {
-	        switch (evt.keyCode) {
-	            case 37:
-	            leftArrowPressed();
-	            break;
-	            case 39:
-	            rightArrowPressed();
-	            break;
-	            case 38:
-	            upArrowPressed();
-	            break;
-	            case 40:
-	            downArrowPressed();
-	            break;
-	            }
-	        };
-
-	    function docReady()
-	    {
-	    	window.addEventListener('keydown', moveSelection);
-	    }
-	}
-
-
 })();
