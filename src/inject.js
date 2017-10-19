dddc1a18b5e5afa18b74858861e5bfaa84a684fb
Alma Wang <almawang96@gmail.com>
2017-10-19 09:03:19 -0400

    websockets

--- src/inject.js
+++ src/inject.js
@@ -1,18 +1,12 @@
 /* eslint no-undef: "off" */
+import * as dataFunctions from './gameData';
 
-import * as GameData from './gameData';
-
-// const URL = 'http://localhost:9090/api';
-// function getHelloMessage() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', `${URL}/`, true);
-//   xhr.onreadystatechange = () => {
-//     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-//       console.log(xhr.response);
-//     }
-//   };
-//   xhr.send();
-// }
 
 console.log('hi');
-GameData.getHelloMessage();
+dataFunctions.getHelloMessage();
+
+const { GameData } = dataFunctions;
+
+const connection = new GameData();
+
+connection.onPlayers(players => console.log(players));
