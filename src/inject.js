0eacb75b278065bc4fac7a9dc4bb9e61d8185587
Alma Wang <almawang96@gmail.com>
2017-10-20 00:03:52 -0400

    removed unused functions

--- src/inject.js
+++ src/inject.js
@@ -1,15 +1,7 @@
 /* eslint no-undef: "off" */
-import * as dataFunctions from './gameData';
 import GameView from './gameView';
 
 console.log('hi');
-// dataFunctions.getHelloMessage();
-
-// const { GameData } = dataFunctions;
-
-// const connection = new GameData();
-
-// connection.onPlayers(players => console.log(players));
 
 const gameView = new GameView();
 gameView.highlightChar(0, 0, 0, 'red');
