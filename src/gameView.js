70adb568451788905e5cdf93dfbd9b2121a8c166
Alma Wang <almawang96@gmail.com>
2017-10-22 15:38:03 -0400

    getMoves

--- src/gameView.js
+++ src/gameView.js
@@ -21,6 +21,74 @@ class GameView {
   highlightChar(sectionId, sentenceId, charId, color = 'yellow') {
     $(this.pageTree[sectionId][sentenceId][charId]).css('background-color', color);
   }
+
+  getMoves(sectionId, sentenceId, charId) {
+    return [this.getUp(sectionId, sentenceId, charId),
+      this.getRight(sectionId, sentenceId, charId),
+      this.getDown(sectionId, sentenceId, charId),
+      this.getLeft(sectionId, sentenceId, charId)];
+  }
+
+  getUp(sectionId, sentenceId, charId) {
+    const offsets = $(this.pageTree[sectionId][sentenceId][charId]).offset();
+    const section = this.pageTree[sectionId];
+    for (let i = sentenceId; i >= 0; i -= 1) {
+      const sentence = section[i];
+      for (let j = i === sentenceId ? charId : sentence.length - 1; j >= 0; j -= 1) {
+        const charOffsets = $(sentence[j]).offset();
+        if (charOffsets.top < offsets.top && charOffsets.left <= offsets.left) {
+          return [sectionId, i, j];
+        }
+      }
+    }
+    return null;
+  }
+
+  getRight(sectionId, sentenceId, charId) {
+    for (let i = sectionId; i < this.pageTree.length; i += 1) {
+      const section = this.pageTree[i];
+      for (let j = i === sectionId ? sentenceId : 0; j < section.length; j += 1) {
+        const sentence = section[j];
+        for (let k = j === sentenceId ? charId : 0; k < sentence.length; k += 1) {
+          if (!(sectionId === i && sentenceId === j && charId === k)) {
+            return [i, j, k];
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+
+  getDown(sectionId, sentenceId, charId) {
+    const offsets = $(this.pageTree[sectionId][sentenceId][charId]).offset();
+    const section = this.pageTree[sectionId];
+    for (let i = sentenceId; i < section.length; i += 1) {
+      const sentence = section[i];
+      for (let j = i === sentenceId ? charId : 0; j < sentence.length; j += 1) {
+        const charOffsets = $(sentence[j]).offset();
+        if (charOffsets.top > offsets.top && charOffsets.left >= offsets.left) {
+          return [sectionId, i, j];
+        }
+      }
+    }
+    return null;
+  }
+
+  getLeft(sectionId, sentenceId, charId) {
+    for (let i = sectionId; i >= 0; i -= 1) {
+      const section = this.pageTree[i];
+      for (let j = i === sectionId ? sentenceId : section.length - 1; j >= 0; j -= 1) {
+        const sentence = section[j];
+        for (let k = j === sentenceId ? charId : sentence.length - 1; k >= 0; k -= 1) {
+          if (!(sectionId === i && sentenceId === j && charId === k)) {
+            return [i, j, k];
+          }
+        }
+      }
+    }
+    return null;
+  }
 }
 
 export default GameView;
--- src/inject.js
+++ src/inject.js
@@ -4,5 +4,15 @@ import GameView from './gameView';
 console.log('hi');
 
 const gameView = new GameView();
-gameView.highlightChar(0, 0, 0, 'red');
-gameView.highlightChar(0, 2, 0);
+gameView.highlightChar(0, 0, 1, 'red');
+const b = gameView.getDown(0, 0, 1);
+gameView.highlightChar(b[0], b[1], b[2], 'blue');
+
+gameView.highlightChar(0, 2, 0, 'green');
+const moves = gameView.getMoves(0, 2, 0);
+moves.forEach((m) => {
+  console.log(m);
+  if (m) {
+    gameView.highlightChar(m[0], m[1], m[2]);
+  }
+});
