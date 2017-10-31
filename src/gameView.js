b2b8ac19243344fd9a2b06a6a89ed32568773b30
Alma Wang <almawang96@gmail.com>
2017-10-31 17:11:55 -0400

    highlights by words

--- src/gameView.js
+++ src/gameView.js
@@ -8,8 +8,8 @@ class GameView {
   createTree() {
     const textElement = $('#mw-content-text').children()[0];
     const sections = $(textElement).children('p').map((x, item) => {
-      const sentenceToCharSpans = sentence => Array.from(sentence).map(char =>
-        `<span>${char}</span>`);
+      const sentenceToCharSpans = sentence => sentence.split(' ').map(word =>
+        `<span>${word} </span>`);
       const sentences = $(item).text().match(/[^.!?]+[.!?]*/g) ? $(item).text().match(/[^.!?]+[.!?]*/g) : [];
       const charSpans = sentences.map(sentenceToCharSpans);
       $(item).html(charSpans.map(sentence => `<span>${sentence.join('')}</span>`).join(''));
