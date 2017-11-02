2ea6a3c9214fe724ab60bfa7de3dc52ff22962b2
Alma Wang <almawang96@gmail.com>
2017-11-02 17:15:46 -0400

    got rid of unnecessary commented code

--- src/inject.js
+++ src/inject.js
@@ -4,23 +4,7 @@ import GameView from './gameView';
 
 console.log('hi');
 
-// const gameView = new GameView();
-// gameView.highlightChar(0, 0, 1, 'red');
-// const b = gameView.getDown(0, 0, 1);
-// gameView.highlightChar(b[0], b[1], b[2], 'blue');
-//
-// gameView.highlightChar(0, 2, 0, 'green');
-// const moves = gameView.getMoves(0, 2, 0);
-// moves.forEach((m) => {
-//   console.log(m);
-//   if (m) {
-//     gameView.highlightChar(m[0], m[1], m[2]);
-//   }
-// });
-
 const gameView = new GameView();
 const gameDriver = new GameDriver(gameView);
 window.addEventListener('keydown', gameDriver.moveSelection);
 GameView.startPopup((username, playerColor) => gameDriver.startGame(username, playerColor));
-
-// gameDriver.startGame();
