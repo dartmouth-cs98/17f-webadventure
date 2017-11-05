01d54e2c2bab9770d846f1ff0138e94664bdca9a
Imanol Avendano <imanol@Imanols-MBP.dartmouth.edu>
2017-11-05 14:22:36 -0500

    rf6

--- src/question.js
+++ src/question.js
@@ -1,5 +1,38 @@
 /* questions.js class */
 
+function badTrail(gameModel, playerId) {
+  return false;
+}
+
+/* generates a question, an answer, and a score */
+function generateQAS(gameModel, trail) {
+  const words = [];
+  let question = '';
+
+  for (let i = 0; i < trail.length; i += 1) {
+    words.append(gameModel.getWord(trail[i]));
+  }
+
+  // eventually should be replaced with questionSelect(words)
+  const select = selectNoun();
+  const answer = words[select];
+  for (let i = 0; i < words.length; i += 1) {
+    if (i === select) {
+      question += '_' * answer.length;
+    } else {
+      question += words[i];
+    }
+  }
+  // scoreQuestion(words)
+  const score = 0;
+
+  return [question, answer, score];
+}
+
+function selectNoun() {
+  return 0;
+}
+
 class Questions {
   constructor(gameModel, playerId) {
     this.gameModel = gameModel;
@@ -24,39 +57,6 @@ class Questions {
 
   score() {
     return this.qas[2];
-  }
-
-  static badTrail(gameModel, playerId) {
-    return false;
-  }
-
-  /* generates a question, an answer, and a score */
-  static generateQAS(gameModel, trail) {
-    const words = [];
-    let question = '';
-
-    for (let i = 0; i < trail.length; i += 1) {
-      words.append(gameModel.getWord(trail[i]));
-    }
-
-    // eventually should be replaced with questionSelect(words)
-    const select = selectNoun();
-    const answer = words[select];
-    for (let i = 0; i < words.length; i += 1) {
-      if (i === select) {
-        question += '_' * answer.length;
-      } else {
-        question += words[i];
-      }
-    }
-    // scoreQuestion(words)
-    const score = 0;
-
-    return [question, answer, score];
-  }
-
-  static selectNoun() {
-    return 0;
   }
 }
 
