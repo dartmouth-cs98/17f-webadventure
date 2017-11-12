4c638925d869fd3a84fc39e0a9dd86eefb54e292
Barry <barry.a.yang.18@dartmouth.edu>
2017-11-12 14:25:48 -0500

    starting

--- src/gameDriver.js
+++ src/gameDriver.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: 0 */
+
 import GameView from './gameView';
 import GameData from './gameData';
 
@@ -91,7 +93,7 @@ class GameDriver {
         const loc = [player.curLocation.sectionID,
           player.curLocation.sentenceID, player.curLocation.character];
         const colorString = `rgb(${player.playerColor.r}, ${player.playerColor.g}, ${player.playerColor.b})`;
-        this.gameView.highlightWord(loc[0], loc[1], loc[2], colorString);
+        // this.gameView.highlightWord(loc[0], loc[1], loc[2], colorString);
       }
     });
     // Danger zone
