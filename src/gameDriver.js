9399b9d724861071c29e178d4dfafc7e5cfa4a0a
Barry <barry.a.yang.18@dartmouth.edu>
2017-11-12 15:46:38 -0500

    endgame popup glitchy

--- src/gameDriver.js
+++ src/gameDriver.js
@@ -7,6 +7,7 @@ const UP = 0;
 const RIGHT = 1;
 const DOWN = 2;
 const LEFT = 3;
+const Q = 100;
 
 class GameDriver {
   constructor(gameView = new GameView()) {
@@ -19,6 +20,8 @@ class GameDriver {
     this.stopMovement = this.stopMovement.bind(this);
     this.makeMove = this.makeMove.bind(this);
     this.moveSelection = this.moveSelection.bind(this);
+
+    this.lastMove = this.nextmove;
     this.getPlayers = this.getPlayers.bind(this);
   }
 
@@ -106,15 +109,28 @@ class GameDriver {
     switch (evt.keyCode) {
       case 65:
         this.nextMove = LEFT;
+        GameView.updateAvatar(this.username, 0);
         break;
       case 68:
         this.nextMove = RIGHT;
+        GameView.updateAvatar(this.username, 1);
         break;
       case 87:
         this.nextMove = UP;
         break;
       case 83:
         this.nextMove = DOWN;
+        break;
+      case 81:
+        this.lastMove = this.nextMove;
+        this.nextMove = Q;
+        this.stopMovement();
+        this.gameView.showPopup();
+        break;
+      case 82:
+        this.gameView.closePopup();
+        this.moveInterval = setInterval(this.makeMove, 250);
+        this.nextMove = this.lastMove;
         break;
       default:
         this.nextMove = this.nextMove;
