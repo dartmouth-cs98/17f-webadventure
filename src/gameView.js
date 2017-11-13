b475a6f0f1305e0900d3fb9f7eec91a45cb74a31
David <davidoh210@gmail.com>
2017-11-13 17:03:50 -0500

    travis fix again

--- src/gameView.js
+++ src/gameView.js
@@ -201,10 +201,10 @@ class GameView {
     players.sort((a, b) => b.curScore - a.curScore);
 
     players.forEach((player, index) => {
-      $(`#top${(index + 1).toString()}`).html(``);
+      $(`#top${(index + 1).toString()}`).html('');
 
       if (player !== undefined && player.curScore > 0) {
-        document.getElementById(`top${(index + 1).toString()}`).style.backgroundColor = `white`;
+        document.getElementById(`top${(index + 1).toString()}`).style.backgroundColor ='white';
         const playerText = `<div>${index + 1}. ${player.username} : ${player.curScore}</div>`;
         $(`#top${(index + 1).toString()}`).html(playerText);
         if (player.username === username) {
