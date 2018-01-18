02303575411d638ee2dc20133008147bddd49705
doh210 <davidoh210@gmail.com>
2018-01-18 15:27:23 -0500

    notify url changes

--- src/background.js
+++ src/background.js
@@ -1,5 +1,21 @@
 /* eslint no-undef: "off" */
 
+var url1 = null;
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+	if (changeInfo.url != undefined) {
+		url1 = changeInfo.url;
+	}
+
+	if (changeInfo.status === 'complete') {
+		alert(url1);
+	}
+
+	//url1 = changeInfo.url;
+
+});
+
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.executeScript(tab.ib, {
     file: 'dist/bundle.js',
