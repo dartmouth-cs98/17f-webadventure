adddcf685ab88efbecf12533513188aee3dde923
sxguo <sandd71@gmail.com>
2018-01-23 17:12:33 -0500

    Redirect url (#163)
    * Added redirect upon pressing enter, executes dist/bundle.js if a valid redirect
    
    * notify url changes
    
    * Fixed travis errors with indentation, etc
    
    * travis fix
    
    * tryint to send url info
    
    * Combined redirect with new non-text-based navigation
    
    * undid changes; merge with redirectUrl
    
    * remove url tracking
    
    * Merge toc with redirect url

--- src/background.js
+++ src/background.js
@@ -1,3 +1,22 @@
+/* eslint no-undef: "off" */
+
+let updatePage = false;
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === 'complete' && updatePage) {
+    updatePage = false;
+    chrome.tabs.executeScript(tabId, {
+      file: 'dist/bundle.js',
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+  // redirect to new url
+  chrome.tabs.update(sender.tab.id, { url: request });
+  updatePage = true;
+});
+
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.executeScript(tab.ib, {
     file: 'dist/bundle.js',
