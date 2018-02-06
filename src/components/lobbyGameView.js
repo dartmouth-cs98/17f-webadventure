6df740e62de4c6a5c231305974ca4bbaaa64594b
Barry <barry.a.yang.18@dartmouth.edu>
2018-02-06 17:47:55 -0500

    lobby components skeleton

---
+++ src/components/lobbyGameView.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+
+class LobbyGameView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hostKey: '',
+      joinKey: '',
+      time: {},
+      seconds: '',
+      // start: false,
+      players: [],
+    };
+
+    this.timer = 0;
+    this.onInputKey = this.onInputKey.bind(this);
+    this.generateKey = this.generateKey.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+    this.checkNumPlayers = this.checkNumPlayers.bind(this);
+    this.addPlayer = this.addPlayer.bind(this);
+  }
+
+  onInputKey(event) {
+    this.setState({
+      joinKey: event.target.value,
+    });
+  }
+
+  generateKey() {
+    const key = Math.random().toString(36).substring(2, 9);
+    this.setState({
+      hostKey: key,
+    });
+  }
+
+  startTimer() {
+    if (this.checkNumPlayers()) {
+      clearInterval(this.timer);
+      this.setState({
+        seconds: 16,
+      });
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    const secs = this.state.seconds - 1;
+    this.setState({
+      time: {
+        s: secs,
+      },
+      seconds: secs,
+    });
+
+    // Check if we're at zero.
+    if (secs === 0) {
+      clearInterval(this.timer);
+      this.setState({
+        time: {
+          s: 'Game start!',
+        },
+      });
+    }
+  }
+
+  checkNumPlayers() {
+    const reqNum = 5;
+    if (this.state.players.length < reqNum) {
+      return false;
+    }
+    return true;
+  }
+
+  addPlayer() {
+    this.setState({
+      players: ['a', 'b', 'c', 'd', 'e', 'f'],
+    });
+  }
+
+  render() {
+    return (
+      <div id="lobby-game-view">
+        <div id="public">
+          <button className="publicGame" onClick={this.startTimer}>
+            Join Public Game
+          </button>
+          {this.state.time.s}
+        </div>
+        <div id="join-private">
+          <input
+            placeholder="Private Game Key"
+            value={this.state.joinKey}
+            onChange={this.onInputKey}
+          />
+          <button className="join" onClick={this.addPlayer}>
+            Join Private Game
+          </button>
+        </div>
+        <div id="host-private">
+          <button onClick={this.generateKey}>
+            Host Private Game
+          </button>
+        </div>
+        <div>{this.state.hostKey}</div>
+      </div>
+    );
+  }
+}
+
+export default LobbyGameView;
