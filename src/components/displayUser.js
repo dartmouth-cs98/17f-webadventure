244e4f2bb6d03fd34a778d887980dea0e8d66086
Barry <barry.a.yang.18@dartmouth.edu>
2018-02-18 18:30:00 -0500

    delete comments

--- src/components/displayUser.js
+++ src/components/displayUser.js
@@ -62,13 +62,6 @@ const DisplayUser = (props) => {
           </div>
         </form>
       </div>
-
-      {/* <div id="avatar-container">
-        <img className="avatar-option" alt="" av_id="0" src="https://i.imgur.com/rZSkKF0.gif" />
-        <img className="avatar-option" alt="" av_id="1" src="https://media.giphy.com/media/jBvHCY91NcurK/giphy.gif" />
-        <img className="avatar-option" alt="" av_id="2" src="https://media.giphy.com/media/jM4bWFBKpSFeo/giphy.gif" />
-        <img className="avatar-option" alt="" av_id="3" src="https://media.giphy.com/media/a9291T1hx1I2I/giphy.gif" />
-      </div> */}
     </div>
   );
 };
