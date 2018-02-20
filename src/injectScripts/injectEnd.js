f191faad7e6cbffdbefa5c3b76697446a60aed5d
Barry <barry.a.yang.18@dartmouth.edu>
2018-02-20 02:23:12 -0500

    working end --separate from this branch lol

--- src/injectScripts/injectEnd.js
+++ src/injectScripts/injectEnd.js
@@ -1,1 +1,9 @@
-console.log('End!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+import End from '../components/end';
+
+console.log('end!');
+$('body').append('<div id=wa-main-end />');
+ReactDOM.render(<End />, document.getElementById('wa-main-end'));
