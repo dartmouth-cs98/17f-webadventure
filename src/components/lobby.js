cb53910b75dcc20b13e9704ceb9b0a082449e91e
Stephanie <sandd71@gmail.com>
2018-02-22 15:40:53 -0500

    Added content to info-box in lobby

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -141,7 +141,12 @@ class Lobby extends Component {
                   <path d="M0 0h24v24H0z" fill="none" />
                   <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
                 </svg>
-                <div id="info-box">Test</div>
+                <div id="info-box">
+                  HOW TO PLAY
+                  <div>Race other WebAdventurers to the goal page and learn more about the world!</div>
+                  <img id="nav-keys" class="info-img" alt="nav-keys" src="https://i.imgur.com/qPjFtPZ.png"/>
+                  <div>Move across the page with WASD and jump links with the L key</div>
+                </div>
               </div>
             </div>
             <div id="lobby-title">WEBADVENTURE</div>
@@ -181,7 +186,12 @@ class Lobby extends Component {
                 <path d="M0 0h24v24H0z" fill="none" />
                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
               </svg>
-              <div id="info-box">Test</div>
+              <div id="info-box">
+                HOW TO PLAY
+                <div>Race other WebAdventurers to the goal page and learn more about the world!</div>
+                <img id="nav-keys" class="info-img" alt="nav-keys" src="https://i.imgur.com/qPjFtPZ.png"/>
+                <div>Move across the page with WASD and jump links with the L key</div>
+              </div>
             </div>
           </div>
           <img src="https://i.imgur.com/VUVNhtC.png" alt="webadventure!" id="webad-logo" />
