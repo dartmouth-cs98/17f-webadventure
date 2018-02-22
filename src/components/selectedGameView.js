cc00d7f980b31d909bc984dbfc4d5cdfbb7e46be
doh210 <davidoh210@gmail.com>
2018-02-22 17:22:28 -0500

    add info to selected game view

--- src/components/selectedGameView.js
+++ src/components/selectedGameView.js
@@ -11,6 +11,9 @@ const SelectedGameView = (props) => {
   return (
     <div id="selectedGameView">
       <div>Playing as {props.avatar}</div>
+      <div>Waiting... {props.selectedGame.players.length}/5 players joined</div>
+      <div>Start: {props.selectedGame.startPage}</div>
+      <div>Players in game:</div>
       {renderPlayers()}
       <button onClick={props.onGoBack}>Go back</button>
     </div>
