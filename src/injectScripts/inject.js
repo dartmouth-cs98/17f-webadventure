56de583571e36126c67c3dcd43080aaa9acf5585
Alma Wang <almawang96@gmail.com>
2018-02-24 12:19:22 -0500

    leaderboard listens to messages

--- src/injectScripts/inject.js
+++ src/injectScripts/inject.js
@@ -1,7 +1,6 @@
 import WikiGame from '../wikiGame';
 import Player from '../player';
 
-let wikiGame;
 
 const onNewUrl = (newUrl) => {
   const req = {
@@ -15,11 +14,11 @@ const onNewUrl = (newUrl) => {
 
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === 'new game') {
-    const { counter, username, avatar } = request.payload;
+    const {
+      counter, username, avatar, game,
+    } = request.payload;
     const curPlayer = new Player(username, avatar);
-    wikiGame = new WikiGame(onNewUrl, curPlayer, counter);
-    wikiGame.updateLeaderboard(request.payload.game);
-  } else if (request.message === 'game info') {
-    wikiGame.updateLeaderboard(request.payload.game);
+    const wikiGame = new WikiGame(onNewUrl, curPlayer, counter, game);
+    console.log(wikiGame);
   }
 });
