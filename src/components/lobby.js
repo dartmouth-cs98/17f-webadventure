1c996eab2bb59f92dc14a50effde9b00b630cfe9
Barry <barry.a.yang.18@dartmouth.edu>
2018-02-23 23:40:44 -0500

    selectedGame renders correctly

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -57,8 +57,17 @@ class Lobby extends Component {
   }
 
   onGames(games) {
-    // need to update selectedGame with new selectedGame
     this.setState({ games });
+
+    // cleaner way to write this?
+    if (this.state.selectedGame) {
+      for (let i = 0; i < games.length; i += 1) {
+        if (this.state.selectedGame.id === games[i].id) {
+          this.setState({ selectedGame: games[i] });
+          break;
+        }
+      }
+    }
   }
 
   onUsers(users) {
@@ -140,28 +149,6 @@ class Lobby extends Component {
       this.setState({ selectedGame: newGame });
     });
   }
-
-  // renderLobbyTop() {
-  //   return (
-  //     <div id="lobby-top">
-  //       <span>
-  //         <button className="exit-lobby-button" onClick={this.exitGame}>&times;</button>
-  //       </span>
-  //       <div>
-  //         <svg id="info" fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-  //           <path d="M0 0h24v24H0z" fill="none" />
-  //           <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
-  //         </svg>
-  //         <div id="info-box">
-  //           HOW TO PLAY
-  //           <div>Race other WebAdventurers to the goal page and learn more about the world!</div>
-  //           <img id="nav-keys" className="info-img" alt="nav-keys" src="https://i.imgur.com/qPjFtPZ.png" />
-  //           <div>Move across the page with WASD and jump links with the L key</div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
 
   renderLowerLeftComponent() {
     if (this.state.selectedGameID && this.state.selectedGame) {
