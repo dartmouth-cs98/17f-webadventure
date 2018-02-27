8f1e11134ac96e7961a2f8a59780442d5084f0f8
byang18 <barry.a.yang.18@dartmouth.edu>
2018-02-27 00:55:39 -0500

    Update signup.js

--- src/components/signup.js
+++ src/components/signup.js
@@ -22,7 +22,7 @@ class SignUp extends Component {
   signInSubmit(event) {
     event.preventDefault();
     if (this.props.allUsers.some(e => e.username === this.state.username)) {
-      this.setState({ errorMsg: 'Sorry! That username is already taken!' });
+      this.setState({ errorMsg: 'Sorry! That username is currently signed in!' });
     } else if (this.state.username === '' || this.state.username.length > 12) {
       this.setState({ errorMsg: 'Please enter a valid username (less than 12 chars)!' });
     } else if (!this.state.username.match(/^[0-9a-zA-Z]+$/)) {
