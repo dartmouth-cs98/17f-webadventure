7dbe57a2035a7a22ac533064213337c24c336a53
doh210 <davidoh210@gmail.com>
2018-03-01 00:46:10 -0500

    timer seems to be working

--- src/components/selectedGameView.js
+++ src/components/selectedGameView.js
@@ -1,30 +1,75 @@
 /* eslint no-mixed-spaces-and-tabs:0, no-tabs:0 */
 
-import React from 'react';
+import React, { Component } from 'react';
 
-const SelectedGameView = (props) => {
-  const renderStartPage = () => {
-    const pageName = decodeURIComponent(props.joinedGame.startPage.split('/').pop()).replace(/_/g, ' ');
+class SelectedGameView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seconds: '',
+    };
+    // this.timer = 0;
+    // this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+    this.renderStartPage = this.renderStartPage.bind(this);
+    this.renderPlayers = this.renderPlayers.bind(this);
+    this.renderTimer = this.renderTimer.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.joinedGame.players.length === 5) {
+      var timer = setInterval(this.countDown, 1000);
+      this.setState({timer: timer, seconds: 5});
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+  }
+
+  countDown() {
+    if (this.state.seconds > 0) {
+      this.setState({seconds: this.state.seconds - 1});
+    } else {
+      clearInterval(this.state.timer);
+    }
+  }
+
+  renderStartPage() {
+    const pageName = decodeURIComponent(this.props.joinedGame.startPage.split('/').pop()).replace(/_/g, ' ');
     return pageName;
   };
 
-  const renderPlayers = () => {
-    return props.joinedGame.players
+  renderPlayers() {
+    return this.props.joinedGame.players
       .map((player) => {
         return (<div>{player.username}</div>);
       });
   };
 
-  return (
-    <div id="selectedGameView">
-      <div>Playing as {props.avatar}</div>
-      <div>{props.joinedGame.players.length}/5 players joined</div>
-      <div>Start: {renderStartPage()}</div>
-      <div>Players in game:</div>
-      {renderPlayers()}
-      <button onClick={props.backToGameSelect}>Go back</button>
-    </div>
-  );
-};
+  renderTimer() {
+    if (this.props.joinedGame.players.length === 5) {
+      return (
+        <div>Starting in {this.state.seconds}</div>
+      )
+    } else {
+      return (<div/>)
+    }
+  };
+
+  render() {
+    return (
+      <div id="selectedGameView">
+        <div>{this.props.joinedGame.players.length}/5 players joined</div>
+        {this.renderTimer()}
+        <div>Start: {this.renderStartPage()}</div>
+        <div>Players in game:</div>
+        {this.renderPlayers()}
+        <button onClick={this.props.backToGameSelect}>Go back</button>
+      </div>
+    );
+  }
+}
 
 export default SelectedGameView;
