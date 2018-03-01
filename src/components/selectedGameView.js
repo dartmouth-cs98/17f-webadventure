c35b62730a1b6937d844877bdf24b941d6019a81
doh210 <davidoh210@gmail.com>
2018-03-01 14:38:38 -0500

    added start game button to private games

--- src/components/selectedGameView.js
+++ src/components/selectedGameView.js
@@ -8,11 +8,14 @@ class SelectedGameView extends Component {
 
     this.state = {
       seconds: '',
+      privateGameStarted: false,
     };
     // this.timer = 0;
     // this.startTimer = this.startTimer.bind(this);
     this.countDown = this.countDown.bind(this);
+    this.startGame = this.startGame.bind(this);
     this.renderStartPage = this.renderStartPage.bind(this);
+    this.renderStartGameButton = this.renderStartGameButton.bind(this);
     this.renderPlayers = this.renderPlayers.bind(this);
     this.renderTimer = this.renderTimer.bind(this);
   }
@@ -21,11 +24,17 @@ class SelectedGameView extends Component {
     if (nextProps.joinedGame.players.length === 5) {
       var timer = setInterval(this.countDown, 1000);
       this.setState({timer: timer, seconds: 5});
+    } else {
+      this.setState({privateGameStarted: false});
     }
   }
 
   componentWillUnmount() {
     clearInterval(this.state.timer);
+  }
+
+  startGame() {
+    this.setState({privateGameStarted: true});
   }
 
   countDown() {
@@ -41,6 +50,20 @@ class SelectedGameView extends Component {
     return pageName;
   };
 
+  renderStartGameButton() {
+    if (this.props.joinedGame.isPrivate) {
+      return (
+        <div>
+          <button onClick={this.startGame}>
+            Start Game
+          </button>
+        </div>
+      );
+    } else {
+      return (<div />)
+    }
+  }
+
   renderPlayers() {
     return this.props.joinedGame.players
       .map((player) => {
@@ -49,7 +72,12 @@ class SelectedGameView extends Component {
   };
 
   renderTimer() {
-    if (this.props.joinedGame.players.length === 5) {
+    if (
+      this.props.joinedGame.players.length === 5 &&
+      (!this.props.joinedGame.isPrivate ||
+        (this.props.joinedGame.isPrivate && this.state.privateGameStarted)
+      )
+    ) {
       return (
         <div>Starting in {this.state.seconds}</div>
       )
@@ -66,6 +94,7 @@ class SelectedGameView extends Component {
         <div>Start: {this.renderStartPage()}</div>
         <div>Players in game:</div>
         {this.renderPlayers()}
+        {this.renderStartGameButton()}
         <button onClick={this.props.backToGameSelect}>Go back</button>
       </div>
     );
