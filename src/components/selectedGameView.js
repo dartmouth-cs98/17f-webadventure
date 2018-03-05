f72aea51b84d86eb29b9557ade24cb1be9473a91
Barry <barry.a.yang.18@dartmouth.edu>
2018-03-05 01:10:01 -0500

    styling changes commited

--- src/components/selectedGameView.js
+++ src/components/selectedGameView.js
@@ -49,30 +49,6 @@ class SelectedGameView extends Component {
     }
   }
 
-<<<<<<< HEAD
-=======
-  onCopy() {
-    this.setState({ copyMsg: 'Copied!' });
-  }
-
-  renderHostKey() {
-    if (this.props.joinedGame.isPrivate) {
-      return (
-        <div>
-          {this.props.joinedGame.id}
-          <CopyToClipboard text={this.props.joinedGame.id}>
-            <button onClick={this.onCopy}>
-              {this.state.copyMsg}
-            </button>
-          </CopyToClipboard>
-        </div>
-      );
-    } else {
-      return (<div />);
-    }
-  }
-
->>>>>>> 874f14196e6da4e8b3fa752fa077fa5ae4f49e3b
   renderStartPage() {
     return decodeURIComponent(this.props.joinedGame.startPage.split('/').pop()).replace(/_/g, ' ');
   }
@@ -134,8 +110,6 @@ class SelectedGameView extends Component {
   }
 
   render() {
-    console.log('joinedGame');
-    console.log(this.props.joinedGame);
     return (
       <div id="selectedGameView">
         {this.renderTimer()}
