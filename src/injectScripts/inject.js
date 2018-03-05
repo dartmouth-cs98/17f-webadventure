35bd9ce2b9c6be73ca0d1a71335bba0576e46932
sxguo <sandd71@gmail.com>
2018-03-05 00:18:28 -0500

    deleted console.logs

--- src/injectScripts/inject.js
+++ src/injectScripts/inject.js
@@ -29,15 +29,10 @@ const exitGame = () => {
 };
 
 chrome.runtime.onMessage.addListener((request) => {
-  console.log('request.payload');
-  console.log(request.payload);
   if (request.message === 'new game') {
     const {
       counter, username, avatar, game, audioOn,
     } = request.payload;
-    console.log('game in inject.js');
-    console.log(game);
-    console.log(game.players);
     const curPlayer = new Player(username, avatar);
     const wikiGame = new WikiGame(onNewUrl, curPlayer, counter, game, audioOn);
     const leaderboard = {
