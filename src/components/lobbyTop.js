0d334e6cf2724f022b71b3a5825585d23cbba43b
doh210 <davidoh210@gmail.com>
2018-03-06 13:41:33 -0500

    make buttons bigger; change lobby position to fit on page

--- src/components/lobbyTop.js
+++ src/components/lobbyTop.js
@@ -10,7 +10,7 @@ const LobbyTop = (props) => {
       </span>
       <div id="sound-lobby" className="sound-on" />
       <div>
-        <svg id="info" fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <svg id="info" fill="#000000" height="37" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" transform="scale(1.5)">
           <path d="M0 0h24v24H0z" fill="none" />
           <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
         </svg>
